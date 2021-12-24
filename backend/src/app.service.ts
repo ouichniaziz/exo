050c7c84be36c1a5d593542bdb3bfc2eab5b9bed
ouichniaziz <medazizouichni@gmail.com>
2021-12-24 01:35:07 +0100

    backend: make a get request for the api call

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs/operators';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    private configService: ConfigService,
+    private httpService: HttpService,
+  ) {}
+  getMovies() {
+    const apiKey = this.configService.get('API_KEY');
+    console.log(apiKey);
+    return this.httpService
+      .get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+      .pipe(map((res) => res.data));
   }
 }
